340d30c7ebcb63044d07440cde8d1524e73847b9
Jared R Clemence <jaredclemence@gmail.com>
2024-01-13 14:18:55 -0800

    Corrected MemberSeeder to ensure no duplicated entries. Also skips creation if database contains certain existing limits.

--- database/seeders/Testing/KCBA/MemberSeeder.php
+++ database/seeders/Testing/KCBA/MemberSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use App\Models\KCBA\Member;
 use App\Models\KCBA\Firm;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\UniqueConstraintViolationException;
 
 class MemberSeeder extends Seeder
 {
@@ -15,9 +16,57 @@ class MemberSeeder extends Seeder
      */
     public function run(): void
     {
-        $factories = Firm::factory()->count(10)->create();
-        $members = Member::factory()
-                ->recycle($factories)
-                ->count(30)->create(/*['firm_id'=>$factories->random()->id]*/);
+        if(Firm::count() < 10){
+            $this->seedFirms();
+        }
+        $firms = Firm::all();
+        
+        $users = [];
+        if(\App\Models\User::count() < 50){
+            $this->seedUsers();
+        }
+        $users = \App\Models\User::all();
+        
+        if( Member::count() < 50 ){
+            $members = [];
+            $users->each(function($user) use ($firms, &$members) {
+                $userid = $user->id;
+                $firmid = $firms->random()->id;
+                $barnum = "";
+                while(strlen($barnum) < 6){
+                    $barnum .= floor(rand() % 10);
+                }
+                $members[] = [
+                    'work_email'=>$user->email,
+                    'user_id' => $userid,
+                    'firm_id' => $firmid,
+                    'barnum'  => $barnum,
+                    'status'  => "ACTIVE"
+                ];
+            });
+            DB::table('members')->insertOrIgnore($members);
+        }
+        
+        assert(Member::count()>=50);
     }
+
+    private function seedFirms() {
+        $firm_names = [];
+        for($i=0; $i<10; $i++){
+            $firm_names[] = ["firm_name"=>fake()->company];
+        }
+        DB::table('firms')->insertOrIgnore($firm_names);
+    }
+
+    private function seedUsers() {
+        for( $i=0; $i<50; $i++){
+            $users[] = [
+                'name'=>fake()->name,
+                'email'=>fake()->email,
+                'password'=> \Illuminate\Support\Facades\Hash::make("password")
+            ];
+        }
+        DB::table('users')->insertOrIgnore($users);
+    }
+
 }
